7db64c9d2850f76e636c4be8395b818bace2ab3e
SOHAIB <jalil.sohaib01@gmail.com>
2023-06-20 18:27:08 +0100

    PART2 COMMIT

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {AccountService} from "../services/account.service";
+import {catchError, Observable, throwError} from "rxjs";
+import {AccountDetails} from "../model/account.model";
 
 @Component({
   selector: 'app-accounts',
   templateUrl: './accounts.component.html',
   styleUrls: ['./accounts.component.css']
 })
-export class AccountsComponent {
+export class AccountsComponent implements OnInit{
+  accountFormGroup! : FormGroup;
+  currentPage : number = 0;
+  pageSize : number = 5;
+  accountObservable! : Observable<AccountDetails>;
+  operationFormGroup! : FormGroup;
+  errorMessage! : String;
 
+  constructor(private fb : FormBuilder, private accountService : AccountService) {
+
+  }
+
+  ngOnInit(): void {
+    this.accountFormGroup = this.fb.group({
+      accountId : this.fb.control('')
+    });
+    this.operationFormGroup = this.fb.group({
+      operationType : this.fb.control(null),
+      amount : this.fb.control(0),
+      description : this.fb.control(null),
+      accountDestination : this.fb.control(null)
+    });
+  }
+
+  handleSearchAccount() {
+    let accountId : String = this.accountFormGroup.value.accountId;
+    this.accountObservable = this.accountService.getAccount(accountId, this.currentPage, this.pageSize).pipe(
+      catchError(err => {
+        this.errorMessage = err.message;
+        return throwError(err);
+      })
+    );
+  }
+
+  gotoPage(page: number) {
+    this.currentPage = page;
+    this.handleSearchAccount();
+  }
+
+  handleAccountOperation() {
+    let accountId : String = this.accountFormGroup.value.accountId;
+    let operationType = this.operationFormGroup.value.operationType;
+    let amount : number = this.operationFormGroup.value.amount;
+    let description : String = this.operationFormGroup.value.description;
+    let accountDestination : String = this.operationFormGroup.value.accountDestination;
+
+    if (operationType == 'DEBIT'){
+      console.log("debit");
+      this.accountService.debit(accountId, amount, description).subscribe({
+        next : (data) => {
+          alert("Success Debit");
+          this.operationFormGroup.reset();
+          this.handleSearchAccount();
+        },
+        error : (err) => {
+          console.log(err);
+        }
+      });
+    }else if (operationType == 'CREDIT'){
+      this.accountService.credit(accountId, amount, description).subscribe({
+        next : (data) => {
+          alert("Success Credit");
+          this.operationFormGroup.reset();
+          this.handleSearchAccount();
+        },
+        error : (err) => {
+          console.log(err);
+        }
+      });
+    }else if (operationType == 'TRANSFER'){
+      this.accountService.transfer(accountId, accountDestination, amount, description).subscribe({
+        next : (data) => {
+          alert("Success Transfer");
+          this.operationFormGroup.reset();
+          this.handleSearchAccount();
+        },
+        error : (err) => {
+          console.log(err);
+        }
+      });
+    }
+  }
 }
--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -4,6 +4,7 @@ import {CustomerService} from "../services/customer.service";
 import {catchError, map, Observable, throwError} from "rxjs";
 import {Customer} from "../model/customer.model";
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-customers',
@@ -14,7 +15,7 @@ export class CustomersComponent implements OnInit{
   customers! : Observable<Array<Customer>>;
   errorMessage! : String;
   searchFormGroup! : FormGroup;
-  constructor(private customerService : CustomerService, private fb : FormBuilder) {
+  constructor(private customerService : CustomerService, private fb : FormBuilder, private router : Router) {
   }
 
   ngOnInit() {
@@ -59,5 +60,9 @@ export class CustomersComponent implements OnInit{
         console.log(err);
       }
     })
+  }
+
+  handleCustomerAccounts(c: Customer) {
+    this.router.navigateByUrl("customer-account/" + c.id, {state : c});
   }
 }
